fb6b12f9ee6797928cde5b5a7364b023422919a5
khalishana <khalisha.hana10@gmail.com>
2023-09-12 21:13:22 +0700

    upload fix

--- main/views.py
+++ main/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Khalisha Hana',
-        'class': 'PBP A'
+        'name': 'Silakan masukkan barang yang ingin disimpan 🥂',
+        'amount': 'Masukkan jumlah barang yang akan disimpan (jumlah barang harus berupa bilangan bulat)',
+        'description' : 'StokNya Aku adalah sebuah web yang dapat menyimpan dan membantumu memantau jumlah barang-barang kesayanganmu 💞'
     }
 
     return render(request, "main.html", context)
